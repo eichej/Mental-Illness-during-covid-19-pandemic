73dd584beaba70a00955d40359ad0b64ccd879f2
eichej <93552209+eichej@users.noreply.github.com>
2021-11-16 13:44:09 -1000

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import os
 os.chdir/Users/mypath').xlsx 
 
-df = pd.read.xls)'\\Users\\josephweichenbaum\\desktop\\mentalillness.xls' 
+df = pd.read.xls)'\\Users\\josephweichenbaum\\desktop\\mentalillness.xlsx' 
                     
 #create a scatter plot with x axis = progressive dates during covid pandemic,
 x = df ['time frame']
@@ -18,6 +18,7 @@ plt.plot(x,y2,label = "% adults w depression")
 plt.plot(x,y1,label = "% adults w anxiety or depression")
 
 plt.show()
+ /.
 
 
 
